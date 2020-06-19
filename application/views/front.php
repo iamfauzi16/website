f883d5e2ae7ae03c3ba3199bf6637401200479e0
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2020-06-19 22:05:14 +0700

    Menambahkan font dan menambahkan tabel dashboard

--- application/views/front.php
+++ application/views/front.php
@@ -7,24 +7,73 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('./assets/style/style.css') ?>">
 
+    <!-- font google dengan cdn -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
     <title>Hello, world!</title>
   </head>
   <body>
   	<!-- Navbar Menu website -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <a class="navbar-brand" href="#">Antrian-e</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
-	  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-	    <div class="navbar-nav">
-	      <a class="nav-item nav-link active" href="#">Tentang Kami <span class="sr-only">(current)</span></a>
-	      <a class="nav-item nav-link" href="#">Cara Penggunaan</a>
-	      <a class="nav-item nav-link" href="#">Kunjungi Web</a>
+    <nav class="navbar  navbar-expand-lg navbar-light bg-light " >
+    	<div class="container text-right">
+			  <a class="navbar-brand font-weight-bold" href="#">Antrian-e</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			    <div class="navbar-nav">
+			      <a class="nav-item nav-link active" href="#">Tentang Kami <span class="sr-only">(current)</span></a>
+			      <a class="nav-item nav-link" href="#">Cara Penggunaan</a>
+			      <a class="nav-item nav-link" href="#">Kunjungi Web</a>
+			      <button class="btn btn-primary mr-3"  type="submit">Login</button>
+			      <button class="btn btn-primary"  type="submit">Daftar</button>
+	  		</div>
 	    </div>
 	  </div>
 	</nav>
+		<!-- Header Website antrian web -->
+		<div class="jumbotron jumbotron-fluid">
+		  <div class="container">
+		    <h1 class="display-4">Hello, Selamat Datang </h1>
+		    <p class="lead">melayani anda dengan sepenuh hati dan menjamin kesehatan anda</p>
+		  </div>
+		</div>
+		<!-- end -->
+		<div class="container">
+			<titleHead>
+				<h1 class="titleUtama text-center"> Silahkan Melihat Antrian Anda</h1>
+			</titleHead>
+			<table class="table table-hover">
+			  <thead>
+			    <tr>
+			      <th scope="col">#</th>
+			      <th scope="col">First</th>
+			      <th scope="col">Last</th>
+			      <th scope="col">Handle</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <th scope="row">1</th>
+			      <td>Mark</td>
+			      <td>Otto</td>
+			      <td>@mdo</td>
+			    </tr>
+			    <tr>
+			      <th scope="row">2</th>
+			      <td>Jacob</td>
+			      <td>Thornton</td>
+			      <td>@fat</td>
+			    </tr>
+			    <tr>
+			      <th scope="row">3</th>
+			      <td colspan="2">Larry the Bird</td>
+			      <td>@twitter</td>
+			    </tr>
+			  </tbody>
+			</table>
+		</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
